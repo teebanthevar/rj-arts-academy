ecda861dba797ede6af6f3d1a78d6e8974829cd7
teebanthevar <teebanthevar@gmail.com>
2026-08-22 12:58:20 +0800

    Improve mobile performance and reduce initial bundle

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,19 @@ import "./analytics";
 import App from "./App.jsx";
 import { StudentProvider } from "./context/StudentContext";
 
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init({
-  duration: 1000,
-  once: true,
-});
+if ("requestIdleCallback" in window) {
+  window.requestIdleCallback(() => {
+    import("aos").then(({ default: AOS }) => {
+      import("aos/dist/aos.css");
+      AOS.init({ duration: 1000, once: true });
+    });
+  });
+} else {
+  setTimeout(() => import("aos").then(({ default: AOS }) => {
+    import("aos/dist/aos.css");
+    AOS.init({ duration: 1000, once: true });
+  }), 1000);
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
